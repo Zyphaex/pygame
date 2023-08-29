a4ed5235d1ef06da53a87fc5d14576bbe2452ddf
Sam Hillier <sam@hillier.uk>
2023-08-29 05:38:23 +0100

    Add colours and file paths constants

--- utilities/constants.py
+++ utilities/constants.py
@@ -1,3 +1,15 @@
+# DISPLAY
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 FPS = 60
+
+# COLOURS
+PURPLE = '#800080'
+PINK = '#FFC0CB'
+
+# FILE PATHS
+FONT_PATH = 'assets/font/Pixeltype.ttf'
+SKY_PATH = 'assets/graphics/sky.png'
+GROUND_PATH = 'assets/graphics/ground.png'
+PLAYER_PATH = 'assets/graphics/player/player_walk_1.png'
+SNAIL_PATH = 'assets/graphics/snail/snail_1.png'
