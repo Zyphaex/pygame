217d80bcea30f67677cad516076412629c004e74
Sam Hillier <sam@hillier.uk>
2023-08-30 17:35:52 +0100

    Scale surfaces to screen

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
 from sys import exit
 
-from utilities.constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, PURPLE, PINK, FONT_PATH, SKY_PATH, GROUND_PATH, PLAYER_PATH, SNAIL_PATH
+from utilities.constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, PURPLE, GROUND_HEIGHT, FONT_PATH, SKY_PATH, GROUND_PATH, PLAYER_PATH, SNAIL_PATH, PLAYER_STAND_PATH
 
 def display_score():
     score = int(pygame.time.get_ticks() / 10) - start_time
     score_surf = font.render(f'{score}', False, PURPLE)
-    score_rect = score_surf.get_rect(center = (400, 50))
+    score_rect = score_surf.get_rect(center=(SCREEN_WIDTH // 2, 60))
     screen.blit(score_surf, score_rect)
     return score
 
@@ -20,12 +20,14 @@ start_time = 0
 hiscore = 0
 
 sky_surf = pygame.image.load(SKY_PATH).convert()
+sky_surf = pygame.transform.scale(sky_surf, (SCREEN_WIDTH, SCREEN_HEIGHT))
 ground_surf = pygame.image.load(GROUND_PATH).convert()
+ground_surf = pygame.transform.scale(ground_surf, (SCREEN_WIDTH, SCREEN_HEIGHT - 300))
 player_surf = pygame.image.load(PLAYER_PATH).convert_alpha()
 snail_surf = pygame.image.load(SNAIL_PATH).convert_alpha()
 
-player_rect = player_surf.get_rect(midbottom = (80, 300))
-snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+player_rect = player_surf.get_rect(midbottom=(100, GROUND_HEIGHT))
+snail_rect = snail_surf.get_rect(midbottom=(SCREEN_WIDTH, GROUND_HEIGHT))
 
 player_gravity = 0
 
@@ -33,13 +35,12 @@ player_gravity = 0
 game_name = font.render('Blob Climbers', False, (111, 196, 169))
 game_name_rect = game_name.get_rect(center = (640, 200))
 
-player_stand = pygame.image.load('assets/graphics/player/player_stand.png').convert_alpha()
-player_stand_rect = player_stand.get_rect(center = (640, 360))
+player_stand = pygame.image.load(PLAYER_STAND_PATH).convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center = (640, 360))
 
 game_message = font.render('Press space to play', False, (111, 196, 169))
-game_message_rect = game_message.get_rect(center = (640, 540))
+game_message_rect = game_message.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 100))
 
 while True:
     for event in pygame.event.get():
@@ -47,42 +48,35 @@ while True:
             pygame.quit()
             exit()
         if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 300:
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= GROUND_HEIGHT:
                 player_gravity = -20
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= GROUND_HEIGHT:
                     player_gravity = -20
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 600
+                snail_rect.left = SCREEN_WIDTH
                 start_time = int(pygame.time.get_ticks() / 10)
 
     if game_active:
         screen.blit(sky_surf, (0, 0))
-        screen.blit(ground_surf, (0, 300))
-        # pygame.draw.rect(screen, PINK, score_rect)
-        # pygame.draw.rect(screen, PINK, score_rect, 10)
-        # screen.blit(score_surf, score_rect)
-        
+        screen.blit(ground_surf, (0, GROUND_HEIGHT))
         # Hiscore
         if display_score() >= hiscore:
             hiscore = display_score()
-
         # Snail
         screen.blit(snail_surf, snail_rect)
         snail_rect.x -= 4
         if snail_rect.right <= 0:
-            snail_rect.left = 600
-
+            snail_rect.left = 1300
         # Player
         player_gravity += 1
         player_rect.y += player_gravity
         screen.blit(player_surf, player_rect)
-        if player_rect.bottom >= 300:
-            player_rect.bottom = 300
+        if player_rect.bottom >= GROUND_HEIGHT:
+            player_rect.bottom = GROUND_HEIGHT
             player_gravity = 0
-
         # Collision
         if snail_rect.colliderect(player_rect):
             game_active = False
@@ -90,9 +84,8 @@ while True:
         screen.fill((94, 129, 162))
         screen.blit(game_name, game_name_rect)
         screen.blit(player_stand, player_stand_rect)
-        score_message = font.render(f'{hiscore}', False, PURPLE)
+        score_message = font.render(f'Hiscore: {hiscore}', False, PURPLE)
         score_message_rect = score_message.get_rect(center = (640, 540))
-        
         if hiscore == 0:
             screen.blit(game_message, game_message_rect)
         else:
--- utilities/constants.py
+++ utilities/constants.py
@@ -7,9 +7,13 @@ FPS = 60
 PURPLE = '#800080'
 PINK = '#FFC0CB'
 
+# MECHANICS
+GROUND_HEIGHT = 600
+
 # FILE PATHS
 FONT_PATH = 'assets/font/Pixeltype.ttf'
 SKY_PATH = 'assets/graphics/sky.png'
 GROUND_PATH = 'assets/graphics/ground.png'
 PLAYER_PATH = 'assets/graphics/player/player_walk_1.png'
 SNAIL_PATH = 'assets/graphics/snail/snail_1.png'
+PLAYER_STAND_PATH = 'assets/graphics/player/player_stand.png'
